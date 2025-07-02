02512774f2e7ef1df2cd89437b52d506cf14c2b3
lhmq24 <quancvl1975@gmail.com>
2025-07-02 08:59:00 +0700

    Add db script

---
+++ shop_db.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2025 at 08:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+-- Error reading structure for table shop_db.accounts: #1932 - Table 'shop_db.accounts' doesn't exist in engine
+-- Error reading data for table shop_db.accounts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shop_db`.`accounts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+-- Error reading structure for table shop_db.carts: #1932 - Table 'shop_db.carts' doesn't exist in engine
+-- Error reading data for table shop_db.carts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shop_db`.`carts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_products`
+--
+-- Error reading structure for table shop_db.cart_products: #1932 - Table 'shop_db.cart_products' doesn't exist in engine
+-- Error reading data for table shop_db.cart_products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shop_db`.`cart_products`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+-- Error reading structure for table shop_db.invoices: #1932 - Table 'shop_db.invoices' doesn't exist in engine
+-- Error reading data for table shop_db.invoices: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shop_db`.`invoices`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table shop_db.products: #1932 - Table 'shop_db.products' doesn't exist in engine
+-- Error reading data for table shop_db.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shop_db`.`products`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
